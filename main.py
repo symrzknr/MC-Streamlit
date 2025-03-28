573698ff60235ad31b7e8b422ab764ac90ad8cba
symrzknr <mrevuelta.deca@gmail.com>
2025-03-28 20:47:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+image1 = r'_Montecarmelo_Octubre2008_V.jpeg'
+image2 = r'98.webp'
+
+st.header("Montecarmelo")
+st.subheader("NO AL CANTON EN MONTECARMELO")
+st.image(image1)
+st.caption("Vista áerea del barrio")
+st.image(image2)
+st.caption("Manifestación 2024")
